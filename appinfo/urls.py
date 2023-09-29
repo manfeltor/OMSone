4e2f598be279783643ea6e0a4dd35296e4f6eb09
felipe <manfeltor@live.com>
2023-09-29 14:03:19 -0300

    logramos poner listado de links en una url de info, vamos a por el boton en navbar

--- appinfo/urls.py
+++ appinfo/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', base, name="home"),
     path('about', about, name="about"),
+    path('index', index, name="index"),
 ]
--- appinfo/views.py
+++ appinfo/views.py
@@ -9,3 +9,7 @@ def base(req):
 def about(req):
 
     return render(req, "about.html")
+
+def index(req):
+
+    return render(req, "index.html")
