c6004e8af13862a1ada2622048b12bf06e61b7eb
felipe <manfeltor@live.com>
2023-10-04 23:21:08 -0300

    ok, HTML esta validando solo las casillas del form, como se hara para darle empty = True? voy a tratar en el form

--- appcompanies/forms.py
+++ appcompanies/forms.py
@@ -5,7 +5,7 @@ class FormCompany(forms.Form):
 
     nombre = forms.CharField(max_length=100)
     rubro = forms.CharField(max_length=20)
-    mail = forms.EmailField(empty_value="vacio")
+    mail = forms.EmailField(empty_value="vacio", error_messages={'invalid':"por favor ingresa un mail valido", 'required' : "por favor, ingresa un mail valido"})
     escliente = forms.BooleanField(required=False)
 
 class FormCompanyM(forms.ModelForm):
